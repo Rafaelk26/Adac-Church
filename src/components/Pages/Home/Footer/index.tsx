40319d5018ed0233c00d064057cb2d5bc4e02ce7
Rafaelk26 <rafaelk262002@gmail.com>
2024-04-16 23:27:26 -0300

    finished footer

--- src/components/Pages/Home/Footer/index.tsx
+++ src/components/Pages/Home/Footer/index.tsx
@@ -11,9 +11,11 @@ export function Footer(){
     return(
         <>
             <footer className="w-full bg-transparent flex flex-wrap h-max bg-gradient-to-b from-black to-gray-600 mb-1">
-                <div className="bg-transparent container mx-auto flex">
+                <div className="bg-transparent container mx-auto flex flex-col items-center 
+                md:flex-row">
                     {/* Logo + subtitle */}
-                    <div className='bg-transparent w-full md:w-2/5 flex flex-col px-4 pt-2'>
+                    <div className='bg-transparent w-full items-center flex flex-col px-4 pt-2
+                    md:items-start md:w-2/5'>
                         <img 
                         className='bg-transparent w-20 h-20'
                         src={adacLogo} 
@@ -22,15 +24,19 @@ export function Footer(){
                     </div>
 
                     {/* Nossas Redes */}
-                    <div className='bg-transparent w-full flex justify-between ps-5
+                    <div className='bg-transparent w-full flex flex-col justify-center
                     sm:flex-col sm:items-center
-                    md:w-3/5 md:border-l-2'>
-                        <div className='bg-transparent w-full md:w-full h-full flex flex-col items-center'>
-                            <h3 className='bg-transparent text-2xl quicksand text-white
-                            sm:w-full
-                            md:w-full'>Nossas Redes</h3>
+                    md:w-3/5 md:border-l-2 md:ps-5 md:items-center'>
+                        <div className='bg-transparent w-full h-full flex flex-col
+                        md:w-full md:items-start'>
+                            <h3 className='bg-transparent text-2xl quicksand text-white flex justify-center
+                            sm:w-full sm:justify-center
+                            md:w-full md:justify-start'>Nossas Redes</h3>
                             {/* Logos */}
-                            <div className='bg-transparent w-full sm:justify-start md:justify-start lg:w-w-full justify-start mt-5 flex gap-3'>
+                            <div className='bg-transparent w-full justify-center flex gap-3
+                            sm:justify-center 
+                            md:justify-start 
+                            lg:w-full'>
                                 <Link to={"/"} className='bg-transparent transition-all hover:scale-110'>
                                     <BiLogoFacebook size={39} color='#0007db' className='bg-transparent' />
                                 </Link>
@@ -46,20 +52,26 @@ export function Footer(){
                             </div>
                         {/* Contatos */}
                         </div>
-                        <div className='bg-transparent w-full flex flex-col justify-end h-full lg:pe-5'>
-                            <h3 className='bg-transparent text-2xl quicksand text-white text-end mt-1'>Contatos</h3>
+                        <div className='bg-transparent w-full flex flex-col justify-end h-full 
+                        lg:pe-5'>
+                            <h3 className='bg-transparent text-2xl quicksand text-white text-center mt-1
+                            md:text-end'>Contatos</h3>
                             {/* Endereço */}
-                            <div className='bg-transparent w-full flex justify-end mt-1'>
-                                <p className='bg-transparent text-white text-end w-1/2 inter'>R. Eng. João Fonseca, 70 - Centro Caraguatatuba - SP, 11660-200</p>
+                            <div className='bg-transparent w-full flex justify-center mt-1
+                            sm:justify-center
+                            md:justify-end'>
+                                <p className='bg-transparent text-white text-end inter'>R. Eng. João Fonseca, 70 - Centro Caraguatatuba - SP, 11660-200</p>
                             </div>
                             {/* Telefone */}
-                            <div className='bg-transparent w-full flex justify-end mt-2'>
-                                <p className='bg-transparent text-white text-end w-1/2 inter'>(12)98139-8573</p>
+                            <div className='bg-transparent w-full flex mt-2 justify-center
+                            md:justify-end'>
+                                <p className='bg-transparent text-white text-end inter'>(12)98139-8573</p>
                             </div>
                             {/* Palavra do senhor */}
-                            <div className='bg-transparent w-full flex flex-col justify-end mt-2'>
-                                <p className='bg-transparent text-white text-end w-full inter'>“Deem graças em todas as circunstâncias, pois esta é a vontade de Deus para vocês em Cristo Jesus.”</p>
-                                <p className='bg-transparent text-white text-end w-full inter font-semibold'>I Tessalonicenses 5:18</p>
+                            <div className='bg-transparent w-full flex flex-col justify-center mt-2
+                            md:w-full md:justify-end'>
+                                <p className='bg-transparent text-white text-end w-full inter md:w-full'>“Deem graças em todas as circunstâncias, pois esta é a vontade de Deus para vocês em Cristo Jesus.”</p>
+                                <p className='bg-transparent text-white w-full text-center inter font-semibold md:text-end'>I Tessalonicenses 5:18</p>
                             </div>
                         </div>
                     </div>
