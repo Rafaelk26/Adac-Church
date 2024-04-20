f42aabe4d764a7e517334112cdfcf25f780a0924
rodrigokferreira <rodrigokferreira2004@gmail.com>
2024-04-20 17:29:04 -0300

    Atualização do componente Slider

--- src/components/Pages/Event/Slide/index.tsx
+++ src/components/Pages/Event/Slide/index.tsx
@@ -1,9 +1,17 @@
-export function Slider(){
-    return(
-        <>
-            <div>
-                <p>teste</p>
-            </div>
-        </>
-    )
+export function Slider() {
+    return (
+        <div style={{ 
+            width: '100%', 
+            height: '300px', 
+            backgroundColor: 'white', 
+            color: 'black', 
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center', 
+            borderRadius: '10px', 
+        }}>
+            {/* Conteúdo do slide aqui */}
+            <p style={{ margin: '0', padding: '0', backgroundColor: 'white', color: 'black', fontSize: '24px', fontWeight: 'bold' }}>Slide de Eventos</p>
+        </div>
+    );
 }
--- src/pages/Eventos/index.tsx
+++ src/pages/Eventos/index.tsx
@@ -2,22 +2,23 @@
 import { ContainerHeader } from '../../components/Container/Header';
 import { ContainerMainCard } from '../../components/Container/MainCard';
 import { HeaderPages } from '../../components/Header/Pages/index';
-import { Slider } from '../../components/Pages/Event/Slide'
+import { Slider } from '../../components/Pages/Event/Slide';
 
-
-export function Eventos(){
-    return(
+// Define a função Eventos
+export function Eventos() {
+    return (
         <>
+            {/* Componente ContainerHeader com o componente HeaderPages dentro */}
             <ContainerHeader>
-                <HeaderPages name='Agenda de Eventos' />
+                <HeaderPages name='Agenda de Eventos' /> 
             </ContainerHeader>
+            {/* Componente ContainerMainCard envolvendo os conteúdos */}
             <ContainerMainCard>
-                <div className='w-full mt-36 flex flex-col justify-center'>
+                <div className='w-full mt-36 flex flex-col md:flex-row items-center'>
                     {/* Introdução */}
-                    <div className='w-1/2 p-5 ps-0 flex justify-start
-                    md:w-1/3'>
-                        <p className='text-sm
-                        md:text-3xl'>
+                    <div className='w-full md:w-1/2 p-5 ps-0 flex justify-start'>
+                        {/* Parágrafo com texto sobre os eventos */}
+                        <p className='text-sm md:text-3xl'>
                             Participe de nossos eventos e descubra oportunidades 
                             emocionantes para se conectar com Deus nessa comunhão 
                             incrível. Junte-se a nós para crescer na fé e no amor. 
@@ -26,12 +27,20 @@ export function Eventos(){
                         </p>
                     </div>
                     {/* Slide */}
-                    <div className='w-1/2
-                    md:w-1/5'>
-                        <Slider />
+                    <div className='w-full md:w-1/2 p-5 flex flex-col justify-center items-center '>
+                        {/* Componente Slider */}
+                        <div className="w-full h-120 md:h-auto mb-5">
+                            <Slider />
+                        </div>
                     </div>
+                </div>
+                {/* Botão "Ver Eventos" */}
+                <div className="flex justify-end">
+                    <button className="bg-black text-white border border-white hover:bg-gray-800 font-bold py-2 px-4 rounded mr-4">
+                        Ver Eventos
+                    </button>
                 </div>
             </ContainerMainCard>
         </>
-    )
+    );
 }
