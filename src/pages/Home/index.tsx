ac92fd10901a3dcc0d582c43bf9cb9af30e2c977
Rafaelk26 <rafaelk262002@gmail.com>
2024-06-06 18:19:46 -0300

    add cache in local storage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,97 +23,108 @@ import backgroundBanner from '../../assets/foto-banner.jpeg';
 // CSS
 import './index.css';
 
-
 // Interface para tipar o objeto de vídeo que retorna da API
-export interface videoProps{
-    title?: string;
-    description?: string;
-    thumbnail?: string;
-    videoId: string;
+export interface videoProps {
+  title?: string;
+  description?: string;
+  thumbnail?: string;
+  videoId: string;
 }
 
-export function Home(){
+const CACHE_KEY = 'videosYouTubeCache';
+const CACHE_DURATION = 60 * 60 * 1000 * 60; 
 
-    // useState são estados, ou melhor dizendo, variáveis que são ativadas e desativadas a qualquer momento
-    // durante a execuçõa do noss o código, aqui criamos uma para armazenar 'true' ou 'false' para nossa função
-    // Se a função retornar 'true' o estado(valor da váriavel) muda e assim por diante.
-    const [exibirImagem, setExibirImagem] = useState<boolean>(false);
-    const [videosYouTube, setVideosYouTube] = useState<videoProps>();
-    
-    // Usamos o useEffect assim que entramos na página e queremos que ele faça algo de imediato.
-    useEffect(()=> {
-        async function fetchVideo(){
-            try{
-                const response = await axios.get('https://adac-church-api.vercel.app/api/videos/');
-                const data: videoProps = response.data[0];
-                setVideosYouTube(data);
-            }
-            catch(err){
-                console.error(`Não foi possível resgatar o vídeo: ${err}`);
-            }
+export function Home() {
+  const [exibirImagem, setExibirImagem] = useState<boolean>(false);
+  const [videosYouTube, setVideosYouTube] = useState<videoProps>();
+
+  useEffect(() => {
+    async function fetchVideo() {
+      const cachedData = localStorage.getItem(CACHE_KEY);
+      const cacheTime = localStorage.getItem(`${CACHE_KEY}_time`);
+
+      if (cachedData && cacheTime) {
+        const age = Date.now() - parseInt(cacheTime, 10);
+        if (age < CACHE_DURATION) {
+          setVideosYouTube(JSON.parse(cachedData));
+          console.log('Dados carregados do cache.');
+          return;
         }
+      }
 
+      try {
+        const response = await axios.get('https://adac-church-api.vercel.app/api/videos/');
+        const data: videoProps = response.data[0];
+        setVideosYouTube(data);
 
-        function handleResizing(){
-            setExibirImagem(window.innerWidth >= 767);
-        }
+        localStorage.setItem(CACHE_KEY, JSON.stringify(data));
+        localStorage.setItem(`${CACHE_KEY}_time`, Date.now().toString());
+        console.log('Dados carregados da API e salvos no cache.');
+      } catch (err) {
+        console.error(`Não foi possível resgatar o vídeo: ${err}`);
+      }
+    }
 
-        window.addEventListener('resize', handleResizing);
-        handleResizing();
-        fetchVideo();
+    function handleResizing() {
+      setExibirImagem(window.innerWidth >= 767);
+    }
 
-        return () => {
-            window.removeEventListener('resize', handleResizing);
-        }
-    },[])
+    window.addEventListener('resize', handleResizing);
+    handleResizing();
+    fetchVideo();
 
-    const memorizedLogoAdac = useMemo(()=> backgroundLogo , []);
+    return () => {
+      window.removeEventListener('resize', handleResizing);
+    };
+  }, []);
 
-    return(
-        <>  
-            {/* Section da foto */}
-            <div className="relative z-20 inset-0">
-                {/* Aplica o gradiente linear transparente para preto dentro da div da imagem */}
-                <div className="absolute z-30 w-full h-full bg-transparent bg-gradient-to-b from-transparent via-transparent to-black"></div>
-                
-                {/* Se o exibir mensagem estiver true, ou seja, ele estiver aqui, essa imagem da logo é renderizada */}
-                {exibirImagem && (
-                    <img 
-                    className='absolute z-30 bg-transparent w-72 top-32 right-0 flex'
-                    src={memorizedLogoAdac} 
-                    alt="ADAC Church" />
-                )}
-                
-                {/* Imagem de fundo da igreja */}
-                <img 
-                src={backgroundBanner}
-                id='imagem_capa' 
-                className='relative w-full h-full object-cover bg-top opacity-65 md:opacity-50'/>
-            </div>
+  const memorizedLogoAdac = useMemo(() => backgroundLogo, []);
 
-            {/* Header Settings */}
-            <ContainerHeader>
-                <HeaderHome  />
-            </ContainerHeader>
+  return (
+    <>
+      {/* Section da foto */}
+      <div className="relative z-20 inset-0">
+        {/* Aplica o gradiente linear transparente para preto dentro da div da imagem */}
+        <div className="absolute z-30 w-full h-full bg-transparent bg-gradient-to-b from-transparent via-transparent to-black"></div>
+        
+        {/* Se o exibir mensagem estiver true, ou seja, ele estiver aqui, essa imagem da logo é renderizada */}
+        {exibirImagem && (
+          <img 
+            className='absolute z-30 bg-transparent w-72 top-32 right-0 flex'
+            src={memorizedLogoAdac} 
+            alt="ADAC Church" />
+        )}
+        
+        {/* Imagem de fundo da igreja */}
+        <img 
+          src={backgroundBanner}
+          id='imagem_capa' 
+          className='relative w-full h-full object-cover bg-top opacity-65 md:opacity-50' />
+      </div>
 
-            {/* Main Settings */}
-            <ContainerMain>
-                <Church />
-                <Ministration
-                link_page='/adac/ministracao/'
-                link={`https://www.youtube.com/embed/${videosYouTube?.videoId}?si=9bVfMuZL3iZzacXc`}
-                />
-                <div 
-                className='flex justify-center flex-col items-center gap-4 mt-16
-                md:flex-row'>
-                    <Cards nome='Jesus'/>
-                    <Cards nome='Alegria'/>
-                    <Cards nome='Fé'/>
-                </div>
-                <Event />
-                <Cell />
-            </ContainerMain>  
-            <Footer />  
-        </>
-    )
+      {/* Header Settings */}
+      <ContainerHeader>
+        <HeaderHome />
+      </ContainerHeader>
+
+      {/* Main Settings */}
+      <ContainerMain>
+        <Church />
+        <Ministration
+          link_page='/adac/ministracao/'
+          link={`https://www.youtube.com/embed/${videosYouTube?.videoId}?si=9bVfMuZL3iZzacXc`}
+        />
+        <div 
+          className='flex justify-center flex-col items-center gap-4 mt-16
+          md:flex-row'>
+          <Cards nome='Jesus' />
+          <Cards nome='Alegria' />
+          <Cards nome='Fé' />
+        </div>
+        <Event />
+        <Cell />
+      </ContainerMain>  
+      <Footer />  
+    </>
+  );
 }
--- src/pages/Ministration/index.tsx
+++ src/pages/Ministration/index.tsx
@@ -1,4 +1,3 @@
-// Import for development
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -14,27 +13,36 @@ import { videoProps } from '../../pages/Home';
 // Logo loading
 import logoLoading from '../../assets/Logo/logo-adac.png';
 
-export function Ministration() {
+const CACHE_KEY = 'youtube_videos_cache';
+const CACHE_DURATION = 60 * 60 * 1000 * 60;
 
+export function Ministration() {
   const [dataVideo, setDataVideo] = useState<videoProps[]>([]);
-  // Loading
   const [isUploading, setIsUploading] = useState(false);
 
-  useEffect(()=> {
+  useEffect(() => {
     const fetchVideo = async () => {
-      try{
+      try {
         setIsUploading(true);
-        const response = await axios.get('https://adac-church-api.vercel.app/api/videos/')
-        setDataVideo(response.data); 
+        const cachedData = localStorage.getItem(CACHE_KEY);
+        const cache = cachedData ? JSON.parse(cachedData) : null;
+
+        if (cache && (Date.now() - cache.timestamp) < CACHE_DURATION) {
+          setDataVideo(cache.data);
+        } else {
+          const response = await axios.get('https://adac-church-api.vercel.app/api/videos/');
+          setDataVideo(response.data);
+          localStorage.setItem(CACHE_KEY, JSON.stringify({ timestamp: Date.now(), data: response.data }));
+        }
+
         setIsUploading(false);
-      }
-      catch(e){
+      } catch (e) {
         console.error(`Não foi possível resgatar os vídeos: ${e}`);
       }
-    }
+    };
 
     fetchVideo();
-  }, [])
+  }, []);
 
   return (
     <>
@@ -49,26 +57,26 @@ export function Ministration() {
         </div>
         {/* Cards Video YouTube */}
         <div className='w-full mt-10 mb-20 flex justify-center items-center gap-5 flex-col'>
-          {dataVideo.map(data=> (
-            <>
-              <CardYouTube
+          {dataVideo.map(data => (
+            <CardYouTube
+              key={data.videoId}
               title={data.title}
               thumbnail={data.thumbnail}
               videoId={data.videoId}
               description={data.description}
-              />
-            </>
+            />
           ))}
         </div>
 
         {/* Div loading */}
         {isUploading && (
           <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50">
-              <img 
+            <img
               className='w-24 fixed'
-              src={logoLoading} 
-              alt="Logo Adac" />
-              <div className="animate-spin rounded-full h-28 w-28 border-t-4 border-b-4 border-white"></div>
+              src={logoLoading}
+              alt="Logo Adac"
+            />
+            <div className="animate-spin rounded-full h-28 w-28 border-t-4 border-b-4 border-white"></div>
           </div>
         )}
       </ContainerMain>
