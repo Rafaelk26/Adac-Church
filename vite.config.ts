4dfd2550a536cfc69494d803b6dfd2752af1e40b
Rafaelk26 <rafaelk262002@gmail.com>
2024-06-07 00:33:05 -0300

    add settings archive for to do deploy in the google.com

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        sitemap: resolve(__dirname, 'sitemap.xml'),
+      },
+    },
+  },
+  server: {
+    // Configuração adicional para servir o sitemap.xml
+    middlewareMode: true,
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    fs: {
+      strict: false,
+    },
+  },
+});
